47cda7d43aa9e10fa8151e45bc28f68e0073f1b5
ImedZa <imed.zayet@edu.isetcom.tn>
2025-04-14 10:32:43 +0100

    Add root endpoint

--- app.py
+++ app.py
@@ -5,14 +5,21 @@ import pandas as pd
 
 app = FastAPI()
 
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Solar API. Use /predict for predictions.  NYL Networks !"}
 # Charger le modèle
 model = joblib.load("solar_model.pkl")
 
 # Définir le format des données d'entrée
+
+
 class InputData(BaseModel):
     AMBIENT_TEMPERATURE: float
     MODULE_TEMPERATURE: float
     IRRADIATION: float
+
 
 @app.post("/predict")
 async def predict(data: InputData):
@@ -22,10 +29,10 @@ async def predict(data: InputData):
         data.MODULE_TEMPERATURE,
         data.IRRADIATION
     ]], columns=["AMBIENT_TEMPERATURE", "MODULE_TEMPERATURE", "IRRADIATION"])
-    
+
     # Faire la prédiction
     prediction = model.predict(input_df)
-    
+
     # Retourner les résultats
     return {
         "DC_POWER": float(prediction[0, 0]),
